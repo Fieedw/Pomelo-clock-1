79df1485f8149e405f47004c01a506b31a332533
fieedw <frankding8843@gmail.com>
2020-07-10 20:35:00 +0800

    init

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com', 
-      app_id: "wx0086239ecbe905e6",
-      app_secret: "5e52208cf66bef7a11c3c41cdd66aa44",
-      t_app_id: "aDBGPqTDa7xj3B5iX6BzKtuc",
-      t_app_secret: "1zVn72m2hcAgNxU1YudeRdgi"
-  }
- })
